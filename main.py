6ddfac39d83c798ee21f1cb763c163123dd3abc5
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-02-02 09:21:47 +0100

    print report

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-
     chars_dict = get_chars_dict(text)
-    print(f"Character count: {chars_dict}")
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    
+    print(f"--- End report ---")
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for item in num_chars_dict:
+        sorted_list.append({"char": item, "num": num_chars_dict[item]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_chars_dict(text):
     text = text.lower()
